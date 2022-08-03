30ef2f9464ef4dbe909332227adfc8755dfee6dc
KH <75588413+khallslearn@users.noreply.github.com>
2022-08-03 10:36:03 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+function findShort(s){
+    var words = s.split(' ');
+    var shortest = words.reduce((shortestWord, currentWord) => {
+      return currentWord.length < shortestWord.length ? currentWord : shortestWord;
+    }, words[0]);
+    return shortest.length;
+  }
